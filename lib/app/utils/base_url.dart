275cf36d8cfc62a4e4809553255ea49025bc8a27
noyon-360 <nazibullahnoyon19.20@gmail.com>
2026-01-21 14:53:10 +0600

    base url update

--- lib/app/utils/base_url.dart
+++ lib/app/utils/base_url.dart
@@ -1,3 +1,4 @@
 // const String baseUrl = "http://192.168.1.100:5000/api";
-const String baseUrl = "http://10.10.5.48:5001/api";
-// const String baseUrl = "https://casaranche-backend-yjla.onrender.com/api";
+const String baseUrl = "https://backend-donaldomitogo.onrender.com/api";
+// const String baseUrl = "http://10.10.5.48:5001/api";
+// const String baseUrl = "https://casaranche-backend-yjla.onrender.com/api"; // client URL
